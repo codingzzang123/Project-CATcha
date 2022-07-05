c257a7ade25c026f2a74c5e35239d03cf12a4a25
호선 <@DESKTOP-9Q7130G.mshome.net>
2022-07-05 23:51:38 +0900

    집에서 수정test

--- src/main/java/com/ib/cat/main/controller/MainController.java
+++ src/main/java/com/ib/cat/main/controller/MainController.java
@@ -1,5 +1,8 @@
-package com.ib.cat.main.controller;
+ package com.ib.cat.main.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MainController {
-
+	/* 추가 */
 }
