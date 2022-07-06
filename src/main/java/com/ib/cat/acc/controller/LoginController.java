8066af0f3d44b5c9349f677442997513412805d5
jiseo <jiseo@192.168.0.13>
2022-07-06 18:03:24 +0900

    7/6 회원가입

--- src/main/java/com/ib/cat/acc/controller/LoginController.java
+++ src/main/java/com/ib/cat/acc/controller/LoginController.java
@@ -3,18 +3,26 @@ package com.ib.cat.acc.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
+import com.ib.cat.acc.member.MemberDao;
 import com.ib.cat.acc.member.MemberDto;
+import com.ib.cat.acc.service.FileService;
 
 @Controller
 public class LoginController {
 	
+	@Autowired
+	FileService fs;
+	@Autowired
+	MemberDao memberDao;
+	@Autowired
+	BCryptPasswordEncoder be;
 
 	@RequestMapping(value="/member/login", method = RequestMethod.GET)
 	public String login() {
@@ -24,20 +32,33 @@ public class LoginController {
 	
 	@RequestMapping(value="/member/login", method = RequestMethod.POST)
 	public String sign(HttpServletRequest req, @Valid MemberDto md, BindingResult bindingResult) {
-//		if(bindingResult.hasErrors()) {
-//			return "member/sign";
-//		}
-		//test 지성
-		MultipartFile mfile = md.getFile();
-		md.setImgo(md.getFile().getOriginalFilename());
-		System.out.println(md.getImgo());
+		if(md.getId()==null) {
+			return "member/login";
+		}
+		if(bindingResult.hasErrors()) {
+			return "member/sign";
+		}
+		String[] img = null;
+		//프로필이 null아닐때
+		if(!md.getFile().isEmpty()) {
+			String path = req.getServletContext().getRealPath("/resources/image/member/");
+			img = fs.fileUpload(md.getFile(), path);
+			md.setImgo(img[0]);
+			md.setImgs(img[1]);			
+		}
+		//프로필이 null일때
+		else {
+			img = fs.nullFileUpload();
+			md.setImgo(img[0]);
+			md.setImgs(img[1]);
+		}
 		
-		System.out.println(req.getServletContext().getRealPath("/"));
-		System.out.println(md.getId());
-		System.out.println(md.getPw());
-		System.out.println(md.getName());
-		System.out.println(md.getEmail());
+		//비밀번호 시큐리티
+		md.setPw(be.encode(md.getPw()));
+		//데이터 저장
+		memberDao.insert(md);
 		
+				
 		return "member/login";
 	}
 	
--- src/main/java/com/ib/cat/acc/controller/SignController.java
+++ src/main/java/com/ib/cat/acc/controller/SignController.java
@@ -1,16 +1,35 @@
 package com.ib.cat.acc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ib.cat.acc.member.MemberDao;
 
 @Controller
 public class SignController {
 	
+	@Autowired
+	MemberDao md;
 	
 	@RequestMapping("member/sign")
 	public String sign() {
 		return "member/sign";
 	}
 	
+	@RequestMapping(value="/member/idCheck", method = {RequestMethod.POST})
+	@ResponseBody
+	public int idCheck(@RequestParam("id") String id) {
+		return md.idCheck(id);
+	}
+	
+	@RequestMapping(value="/member/nameCheck", method = {RequestMethod.POST})
+	@ResponseBody
+	public int nameCheck(@RequestParam("name") String name) {
+		return md.nameCheck(name);
+	}
 	
 }
